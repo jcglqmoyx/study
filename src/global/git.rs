5dcbefb1170d485f2e085e8813bab7af17671900
hqc <jcglqmoyx@gmail.com>
2024-09-18 19:24:25 -0400

    update

--- src/global/git.rs
+++ src/global/git.rs
@@ -1,2 +1,2 @@
-pub(crate) const GIT_REMOTE_REPOSITORY_URL: &str = "git@gitee.com:jcglqmoyx/task.git";
+pub(crate) const GIT_REMOTE_REPOSITORY_URL: &str = "git@git.acwing.com:jcglqmoyx/task.git";
 pub(crate) const GIT_REPOSITORY_NAME: &str = "task";
